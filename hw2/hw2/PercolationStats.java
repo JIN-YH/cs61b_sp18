e0190ec0f045976ada0b1b425ae5c41c6325ba5c
JIN-YH <82925563+JIN-YH@users.noreply.github.com>
2021-06-01 22:40:53 +0800

    Add files via upload

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,4 +1,5 @@
 package hw2;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.introcs.StdRandom;
 import edu.princeton.cs.introcs.StdStats;
 
@@ -6,7 +7,6 @@ public class PercolationStats {
     private int N;
     private int T;
     private double[] X;
-    private Percolation P;
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
             throw new java.lang.IllegalArgumentException("the input is out of bound");
@@ -14,8 +14,8 @@ public class PercolationStats {
         X = new double[T];
         this.N = N;
         this.T = T;
-        P = pf.make(N);
         for (int i = 0; i < T; i++) {
+            Percolation P = pf.make(N);
             while (!P.percolates()) {
                 P.open(StdRandom.uniform(N), StdRandom.uniform(N));
             }
