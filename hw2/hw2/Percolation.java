4dfcb39db4d664ee92a2103bd0793e926c8e9a76
JIN-YH <82925563+JIN-YH@users.noreply.github.com>
2021-06-01 22:24:08 +0800

    Add files via upload

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,89 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int N;
+    private WeightedQuickUnionUF Union1;
+    private WeightedQuickUnionUF Union2;
+    private int[][] Pos;
+    private int count;
+    private int low;
+    private int high;
+    public Percolation(int N) {
+        high = N * N;
+        low = high + 1;
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException("N must be bigger than 0");
+        }
+        this.N = N;
+        Pos = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                Pos[i][j] = 0;
+            }
+        }
+        Union1 = new WeightedQuickUnionUF(N * N + 2);
+        Union2 = new WeightedQuickUnionUF(N * N + 1);
+        count = 0;
+    }
+    private int xyto2D(int row, int col) {
+        return row * N + col;
+    }
+
+    public void open(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new java.lang.IndexOutOfBoundsException("row or col is out of bound");
+        }
+        Pos[row][col] = 1;
+        count += 1;
+        if (row == 0) {
+            Union1.union(xyto2D(row, col), high);
+            Union2.union(xyto2D(row, col), high);
+        }
+        if (row == N - 1) {
+            Union1.union(xyto2D(row, col), low);
+        }
+        update(row, col, row - 1, col);
+        update(row, col, row + 1, col);
+        update(row, col, row, col + 1);
+        update(row, col, row, col - 1);
+    }
+
+    private void update(int row, int col, int newrow, int newcol) {
+        if (newrow < 0 || newrow >= N || newcol < 0 || newcol >= N) {
+            return;
+        }
+        if (Pos[newrow][newcol] == 1) {
+            Union1.union(xyto2D(row, col), xyto2D(newrow, newcol));
+            Union2.union(xyto2D(row, col), xyto2D(newrow, newcol));
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new java.lang.IndexOutOfBoundsException("row or col is out of bound");
+        }
+        return Pos[row][col] == 1;
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new java.lang.IndexOutOfBoundsException("row or col is out of bound");
+        }
+        if (Union2.connected(xyto2D(row, col), high)) {
+            return true;
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        return count;
+    }
+
+    public boolean percolates() {
+        if (Union1.connected(low, high)) {
+            return true;
+        }
+        return false;
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,42 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
+    private int N;
+    private int T;
+    private double[] X;
+    private Percolation P;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException("the input is out of bound");
+        }
+        X = new double[T];
+        this.N = N;
+        this.T = T;
+        P = pf.make(N);
+        for (int i = 0; i < T; i++) {
+            while(!P.percolates()) {
+                P.open(StdRandom.uniform(N), StdRandom.uniform(N));
+            }
+            X[i] =(double) P.numberOfOpenSites() / (N * N);
+        }
 
+    }
+
+    public double mean() {
+        return StdStats.mean(X);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(X);
+    }
+
+    public double confidenceLow() {
+        return this.mean() - 1.96 * this.stddev() / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return this.mean() + 1.96 * this.stddev() / Math.sqrt(T);
+    }
 }
