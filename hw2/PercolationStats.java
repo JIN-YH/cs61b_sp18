760bc2f96f3588e97f5a61b77f0d903fe5934da2
JIN-YH <82925563+JIN-YH@users.noreply.github.com>
2021-06-01 22:43:41 +0800

    Add files via upload

---
+++ hw2/PercolationStats.java
@@ -0,0 +1,43 @@
+package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
+public class PercolationStats {
+    private int N;
+    private int T;
+    private double[] X;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException("the input is out of bound");
+        }
+        X = new double[T];
+        this.N = N;
+        this.T = T;
+        for (int i = 0; i < T; i++) {
+            Percolation P = pf.make(N);
+            while (!P.percolates()) {
+                P.open(StdRandom.uniform(N), StdRandom.uniform(N));
+            }
+            X[i] = (double) P.numberOfOpenSites() / (N * N);
+        }
+
+    }
+
+    public double mean() {
+        return StdStats.mean(X);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(X);
+    }
+
+    public double confidenceLow() {
+        return this.mean() - 1.96 * this.stddev() / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return this.mean() + 1.96 * this.stddev() / Math.sqrt(T);
+    }
+
+
+}
